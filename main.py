3cf73f0c1321e98f382b870abe4c898182d78e4e
Vyrrhus <baptistedesoubrie@gmail.com>
2018-10-14 23:07:56 +0200

    Del useless extensions

--- main.py
+++ main.py
@@ -5,30 +5,17 @@ BOT DE MODÉRATION DU DISCORD INSOUMIS
 
 FILE MAP
 --------
-MAIN >>>>>>>>>> bot.py 			 : 	gère les extensions & commandes de staff
-
-EXTENSIONS >>>>	moderation.py 	 :	commandes & events pour la modération
-				miscellaneous.py :	commandes utilitaires (principalement pour salons vocaux)
-				pin.py			 :	commandes & events pour automatiser les unpin dans le temps
-				twitter.py 		 :  commandes pour hook en continu certains tweets
+MAIN >>>>>>>>>> main.py 		: 	gère les extensions & commandes de staff
+				twitter.py		:	task pour suivre des tweets & commandes pour interagir
 				
-CLASS >>>>>>>>> src/action.py	 :	classe ACTION (infos relatives à la modération)
-				src/note.py		 :	classe NOTE (idem)
-			
-FUNCTION >>>>>>	src/check.py	 :	fonctions renvoyant un boolean pour vérifier certaines conditions
-				src/tools.py	 :	fonctions utilitaires pour sérialiser / désérialiser des fichiers
+SRC >>>>>>>>>>> src/check.py	:	fonctions renvoyant un boolean pour vérifier certaines conditions
+				src/tool.py		:	fonctions utilitaires pour réaliser des conversions, sérialiser, etc.
+				src/tweetool.py	:	fonctions utilitaires spécifiques au module twitter.py
 				
-FILES >>>>>>>>> src/config/settings.py	:	charge le fichier json associé dans une variable data
-				src/config/settings.json:	contient les paramètres du bot (ID, tokens, texte, etc.)
-				src/data/counter.txt	:	compteur du nombre d'objets de ACTION
-				src/data/log			:	binary file contenant un dict des ACTION par numéro
-				src/data/member			:	binary file contenant un dict des numéros d'actions & notes par ID
-
+	CONFIG >>>>>>>>>>> src/config/settings.py : charge les fichiers json dans les variables associées
+				
 WIP
 ---
-- supprimer !close
-- rajouter les commandes staff pour modifier settings.json
-- commande !clearnote
 - vérifier qu'on ne peut pas modérer un autre bot (comment?)
 - changer !help commande (decorators ?)
 - assert toutes les fonctions?
@@ -62,8 +49,11 @@ async def on_ready():
 
 @client.before_invoke
 async def before_any_command(ctx):
-	"""Global hook pour interrompre les commandes (bot capricieux)"""
-	
+	"""Global hook pour interrompre les commandes (bot capricieux)
+		>>> Désactivé for now
+	"""
+	if True:
+		return
 	ctx.global_lock = False
 	probability = 0.01
 	if random.random() > probability:
@@ -75,6 +65,7 @@ async def before_any_command(ctx):
 	
 @client.command()
 async def close(ctx):
+	"""Owner can log off"""
 	if ctx.message.author.id == 246321888693977088:
 		# Saving data
 		tool.set_data(data, 'src/config/settings.json')
@@ -92,9 +83,6 @@ async def load(ctx, extension):
 	"""Charge une extension : ?load <extension>
 	
 	Extensions accessibles :
-	- 'moderation'
-	- 'miscellaneous'
-	- 'pin'
 	- 'twitter'
 	"""
 	if not check.is_staff(ctx.author):
@@ -110,9 +98,6 @@ async def unload(ctx, extension):
 	"""Retire l'extension : ?unload <extension>
 	
 	Extensions accessibles :
-	- 'moderation'
-	- 'miscellaneous'
-	- 'pin'
 	- 'twitter'
 	"""
 	if not check.is_staff(ctx.author):
