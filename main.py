423c13552a33c1dc075d44eaadc4e4d995a1cb6a
Vyrrhus <baptistedesoubrie@gmail.com>
2018-10-24 15:10:26 +0200

    edit version

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ from src.config.settings import data, client
 
 # GLOBAL SETTINGS
 TOKEN = data['TOKEN']
-VERSION = '1.0c'
+VERSION = '1.0d'
 
 # EXTENSIONS : loaded by default
 extensions = ['twitter']
