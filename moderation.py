3d36b3d1483e7085d190c78c922eb7244e4e3f78
Vyrrhus <baptistedesoubrie@gmail.com>
2018-12-08 16:02:49 +0100

    Monitoring + fix minor issues

--- moderation.py
+++ moderation.py
@@ -1,6 +1,5 @@
 import discord
 import asyncio
-import random
 from datetime import datetime
 from datetime import timedelta
 
@@ -8,22 +7,26 @@ import src.check as check
 import src.tool as tool
 from src.tool import log
 
+from src.config.settings import MONITOR_MODERATION as MNT
+from src.config.settings import GUILD_ID
 from discord.ext import commands
-from src.config.settings import data
 from src.action import ACTION
 #from src.note import NOTE
 
 class Moderation:
 	def __init__(self, client):
 		self.client = client
+		# Store id & text for events
+		self.id = tool.get_data('src/config/id.json')
+		self.text = tool.get_data('src/config/moderation/text.json')
+		self.sdd_role = self.id['ROLE']['DISCU'] + self.id['ROLE']['DIALO']
+		# Exit lock to block event triggered by command
 		self.exit_lock = False
 		
 	###########################################
 	#            COROUTINE TASK               #
-	###########################################	
-	
+	###########################################
 	"""No task in this module"""
-	
 	
 	###########################################
 	#                  CHECKS                 #
@@ -35,9 +38,9 @@ class Moderation:
 			En outre, elles ne sont utilisables que dans le salon de modération
 		"""
 		try:
-			return check.is_moderator(ctx.author) and check.in_moderation_channel(ctx.channel)
+			return check.is_role(ctx.author, self.id['ROLE']['MODER']) and check.in_channel(ctx.channel, self.id['TEXTCHANNEL']['MODER'])
 		except:
-			await log('moderation local check failed')
+			await log('moderation local check failed', MNT)
 			return False
 		
 	###########################################
@@ -56,8 +59,8 @@ class Moderation:
 		""" Envoie un warn à l'user
 		"""
 		# Checks
-		if check.is_moderator(target):
-			await log('WARN - target role invalide')
+		if check.is_role(target, self.id['ROLE']['MODER']):
+			await log('WARN - target role invalide', MNT)
 			return
 		
 		# Action
@@ -65,11 +68,11 @@ class Moderation:
 		EMB = action.embed(self.client, 0xaa8800)
 		
 		# Notification aux modérateurs
-		await self.client.get_channel(data['ID']['SALON_LOG']).send(content=None, embed=EMB)
+		await self.client.get_channel(self.id['TEXTCHANNEL']['LOG']).send(content=None, embed=EMB)
 		await ctx.channel.send('{} a reçu un warn :telephone:'.format(target.mention))
 		
 		# Envoi du warn
-		await target.send(data['TEXT']['WARN'].format(text))
+		await target.send(self.text['WARN'].format(text))
 		
 		# Sauvegarde du log
 		pass
@@ -80,20 +83,21 @@ class Moderation:
 		""" Kick un user (message optionnel)
 		"""
 		# Checks
-		if check.is_moderator(target):
-			await log('KICK - target role onvalide')
+		if check.is_role(target, self.id['ROLE']['MODER']):
+			await log('KICK - target role onvalide', MNT)
 			return
 		if not check.kick_allowed(target, ctx.message.author) or not check.kick_allowed(target, ctx.guild.me):
-			await log('KICK - permission de kick refusée')
+			await log('KICK - permission de kick refusée', MNT)
 			return
 		
 		# Séparation du text en message + auditlog
 		message, auditlog = tool.extract_text(text)
+		print('message: {}\nauditlog: {}'.format(message, auditlog))
 		if not auditlog:
-			await log('KICK - auditlog not given')
+			await log('KICK - auditlog not given', MNT)
 			return await ctx.channel.send("Auditlog manquant : ?kick [message envoyé] auditlog")
 		if len(auditlog) > 512:
-			await log('KICK - auditlog too long (512 char max)')
+			await log('KICK - auditlog too long (512 char max)', MNT)
 			return await ctx.channel.send("Auditlog trop long *({}/512 caractères max)*".format(len(auditlog)))
 		
 		# ACTION
@@ -101,14 +105,15 @@ class Moderation:
 		EMB = action.embed(self.client, 0x995500)
 		
 		# Notification aux modérateurs
-		await self.client.get_channel(data['ID']['SALON_LOG']).send(content=None, embed=EMB)
+		await self.client.get_channel(self.id['TEXTCHANNEL']['LOG']).send(content=None, embed=EMB)
 		await ctx.channel.send('{} a été kick du serveur :hammer:'.format(target.mention))
 		
 		# Kick
 		if message:
-			await target.send(data['TEXT']['KICK']['with_msg'].format(message))
+			await target.send(self.text['KICK'].format(message))
 		else:
-			await target.send(data['TEXT']['KICK']['with_no_msg'])
+			await target.send(self.text['KICK_w_msg'])
+		self.exit_lock = True
 		await target.kick(reason=auditlog)
 		
 		# Sauvegarde du log
@@ -126,7 +131,7 @@ class Moderation:
 	async def on_member_join(self, member):
 		""" Vérifie les logs des nouveaux arrivants
 		"""
-		await log('{} a rejoint le serveur'.format(member.name), time=True)
+		await log('{} a rejoint le serveur'.format(member.name), MNT, time=True)
 				
 	# ON MEMBER REMOVE	
 	async def on_member_remove(self, member):
@@ -139,74 +144,74 @@ class Moderation:
 		  Pour y palier, on a mis un délai de 5s avant lecture de l'auditlog, puis on récupère les 20 dernières entrées (en supposant en cas de raid jusqu'à 20 kick / bans en 5s) pour vérifier si l'une d'entre elle correspond.
 		  C'est à dire si l'entrée a été faite il y a moins de 5s, correspond à un kick / ban et concerne l'utilisateur souhaité.
 		"""
-		await log('{} a quitté le serveur'.format(member.name), time=True)
+		await log('{} a quitté le serveur'.format(member.name), MNT, time=True)
 		
 		# Commande utilisée:
 		if self.exit_lock:
 			self.exit_lock = False
-			return await log('verrou actif: commande')
+			return await log('verrou actif: commande', MNT)
 		
-		guild = self.client.get_guild(data['ID']['GUILD'])
+		guild = self.client.get_guild(GUILD_ID)
 		if not check.auditlog_allowed(guild.me):
-			return await log("HAL 9000 n'a pas accès à l'auditlog")
+			return await log("HAL 9000 n'a pas accès à l'auditlog", MNT)
 		await asyncio.sleep(5)
 		
 		# Kick
 		async for entry in guild.audit_logs(action=discord.AuditLogAction.kick, limit=20):
-			await log('KICK : auditlog timer')
-			await log(str(datetime.utcnow()-entry.created_at))
-			if datetime.utcnow() - entry.created_at < timedelta(0,10):
+			if datetime.utcnow() - entry.created_at > timedelta(0,10):
 				break
+			print(str(entry.target))
 			if entry.target == member:
-				await log("KICK manuel")
+				await log("KICK manuel", MNT)
 				action = ACTION('Kick', entry.target, entry.user, entry.created_at, reason=entry.reason)
 				EMB = action.embed(self.client, 0x995500)
-				await self.client.get_channel(data['ID']['SALON_LOG']).send(content=None, embed=EMB)
-				await self.client.get_channel(data['ID']['SALON_MODERATION']).send(':hammer: {} a été kick du serveur.'.format(str(entry.target)))
+				await self.client.get_channel(self.id['TEXTCHANNEL']['LOG']).send(content=None, embed=EMB)
+				await self.client.get_channel(self.id['TEXTCHANNEL']['MODER']).send(':hammer: {} a été kick du serveur.'.format(str(entry.target)))
 				
 				# Check SDD
-				if check.is_sdd(member):
+				if check.is_role(member, self.sdd_role):
 					for role in member.roles:
-						if role.id == data['ID']['ROLE_DISCUSSION'] and not role.members:
-							channel = self.client.get_channel(data['ID']['SALON_DISCUSSION'])
+						if role.id == self.id['ROLE']['DISCU'] and not role.members:
+							channel = self.client.get_channel(self.id['TEXTCHANNEL']['DISCU'])
 							await channel.send('```LIBRE```')
-						if role.id == data['ID']['ROLE_DIALOGUE'] and not role.members:
-							channel = self.client.get_channel(data['ID']['SALON_DIALOGUE'])
+						if role.id == self.id['ROLE']['DIALO'] and not role.members:
+							channel = self.client.get_channel(self.id['TEXTCHANNEL']['DIALO'])
 							await channel.send('```LIBRE```')
 				return
 		
 		# Ban
 		async for entry in guild.audit_logs(action=discord.AuditLogAction.ban, limit=20):
-			if datetime.utcnow() - entry.created_at < timedelta(0,10):
+			if datetime.utcnow() - entry.created_at > timedelta(0,10):
 				break
 			if entry.target == member:
-				await log('BAN manuel')
+				await log('BAN manuel', MNT)
 				action = ACTION('Ban', entry.target, entry.user, entry.created_at, reason=entry.reason)
 				EMB = action.embed(self.client, 0x550000)
-				await self.client.get_channel(data['ID']['SALON_LOG']).send(content=None, embed=EMB)
-				await self.client.get_channel(data['ID']['SALON_MODERATION']).send(':skull_crossbones: {} a été ban du serveur.'.format(str(entry.target)))
+				await self.client.get_channel(self.id['TEXTCHANNEL']['LOG']).send(content=None, embed=EMB)
+				await self.client.get_channel(self.id['TEXTCHANNEL']['MODER']).send(':skull_crossbones: {} a été ban du serveur.'.format(str(entry.target)))
 				
 				# Check SDD
-				if check.is_sdd(member):
+				if check.is_role(member, self.sdd_role):
 					for role in member.roles:
-						if role.id == data['ID']['ROLE_DISCUSSION'] and not role.members:
-							channel = self.client.get_channel(data['ID']['SALON_DISCUSSION'])
+						if role.id == self.id['ROLE']['DISCU'] and not role.members:
+							channel = self.client.get_channel(self.id['TEXTCHANNEL']['DISCU'])
 							await channel.send('```LIBRE```')
-						if role.id == data['ID']['ROLE_DIALOGUE'] and not role.members:
-							channel = self.client.get_channel(data['ID']['SALON_DIALOGUE'])
+						if role.id == self.id['ROLE']['DIALO'] and not role.members:
+							channel = self.client.get_channel(self.id['TEXTCHANNEL']['DIALO'])
 							await channel.send('```LIBRE```')
 				return
 		
 		# Départ volontaire
-		if check.is_sdd(member):
+		await log('Départ volontaire', MNT)
+		if check.is_role(member, self.sdd_role):
 			for role in member.roles:
-				if role.id == data['ID']['ROLE_DISCUSSION'] and not role.members:
-					channel = self.client.get_channel(data['ID']['SALON_DISCUSSION'])
+				if role.id == self.id['ROLE']['DISCU'] and not role.members:
+					channel = self.client.get_channel(self.id['TEXTCHANNEL']['DISCU'])
 					await channel.send('```LIBRE```')
-				if role.id == data['ID']['ROLE_DIALOGUE'] and not role.members:
-					channel = self.client.get_channel(data['ID']['SALON_DIALOGUE'])
+				if role.id == self.id['ROLE']['DIALO'] and not role.members:
+					channel = self.client.get_channel(self.id['TEXTCHANNEL']['DIALO'])
 					await channel.send('```LIBRE```')
-			await self.client.get_channel(data['ID']['SALON_MODERATION']).send(':no_pedestrians: {} a pris la fuite de la SDD !'.format(member.mention))
+			await self.client.get_channel(self.id['TEXTCHANNEL']['MODER']).send(':no_pedestrians: {} a pris la fuite de la SDD !'.format(member.mention))
 		
 		
 	# ON RAW REACTION ADD
@@ -232,16 +237,18 @@ class Moderation:
 		"""
 		
 		# Mise en SDD
-		if not check.is_sdd(before) and check.is_sdd(after):
-			await log('{} a été mis en SDD'.format(before.name), time=True)
+		if not check.is_role(before, self.sdd_role) and check.is_role(after, self.sdd_role):
+			await log('{} a été mis en SDD'.format(before.name), MNT, time=True)
 			return
-		elif check.is_sdd(before) and not check.is_sdd(after):
+		elif check.is_role(before, self.sdd_role) and not check.is_role(after, self.sdd_role):
 			lost_role = [role for role in before.roles if role not in set(after.roles)]
-			await log('{} a perdu le rôle {}'.format(after.name, lost_role[0].name))
-			if lost_role[0].id == data['ID']['ROLE_DIALOGUE']:
-				channel = self.client.get_channel(data['ID']['SALON_DIALOGUE'])
+			await log('{} a perdu le rôle {}'.format(after.name, lost_role[0].name), MNT)
+			print(lost_role[0].id)
+			if lost_role[0].id in self.id['ROLE']['DIALO']:
+				channel = self.client.get_channel(self.id['TEXTCHANNEL']['DIALO'])
 			else:
-				channel = self.client.get_channel(data['ID']['SALON_DISCUSSION'])
+				channel = self.client.get_channel(self.id['TEXTCHANNEL']['DISCU'])
+			
 			if not lost_role[0].members:
 				return await channel.send('```LIBRE```')
 		else:
