9b14202afafea2013bae5f606f5b1f5e73ca4d0d
Vyrrhus <baptistedesoubrie@gmail.com>
2018-12-09 00:11:22 +0100

    Add log, clearlog commands & events

--- moderation.py
+++ moderation.py
@@ -64,7 +64,7 @@ class Moderation:
 			return
 		
 		# Action
-		action = ACTION('Warn', target, ctx.message.author, ctx.message.created_at, message=text)
+		action = ACTION('Warn', target.id, ctx.message.author.id, ctx.message.created_at, message=text)
 		EMB = action.embed(self.client, 0xaa8800)
 		
 		# Notification aux modérateurs
@@ -97,10 +97,10 @@ class Moderation:
 			return await ctx.channel.send("Auditlog manquant : ?kick [message optionnel] auditlog")
 		if len(auditlog) > 512:
 			await log('KICK - auditlog too long (512 char max)', MNT)
-			return await ctx.channel.send("Auditlog trop long *({}/512 caractères max)*".format(len(auditlog)))
+			return await ctx.channel.send("Auditlog trop long (`{}/512` caractères max)".format(len(auditlog)))
 		
 		# ACTION
-		action = ACTION('Kick', target, ctx.message.author, ctx.message.created_at, message=message, reason=auditlog)
+		action = ACTION('Kick', target.id, ctx.message.author.id, ctx.message.created_at, message=message, reason=auditlog)
 		EMB = action.embed(self.client, 0x995500)
 		
 		# Notification aux modérateurs
@@ -137,10 +137,10 @@ class Moderation:
 			return await ctx.channel.send('Auditlog manquant : ?ban [message optionnel] auditlog')
 		if len(auditlog) > 512:
 			await log('BAN - auditlog too long (512 char max)', MNT)
-			return await ctx.channel.send("Auditlog trop long *({}/512 caractères max)".format(len(auditlog)))
+			return await ctx.channel.send("Auditlog trop long (`{}/512` caractères max)".format(len(auditlog)))
 		
 		# ACTION
-		action = ACTION('Ban', target, ctx.message.author, ctx.message.created_at, message=message, reason=auditlog)
+		action = ACTION('Ban', target.id, ctx.message.author.id, ctx.message.created_at, message=message, reason=auditlog)
 		EMB = action.embed(self.client, 0x550000)
 		
 		# Notification aux modérateurs
@@ -158,6 +158,60 @@ class Moderation:
 		# Sauvegarde du log
 		action.save('src/config/moderation/data.json')
 		
+	@commands.command(name='log', pass_context=True)
+	async def log(self, ctx, target:discord.User):
+		""" Récupère les logs d'un user
+		"""
+		# Checks
+		if check.is_role(target, self.id['ROLE']['MODER']):
+			await log('LOG - target role invalide', MNT)
+			return
+		
+		# Obtention des logs
+		logs = tool.get_data('src/config/moderation/data.json')
+		list_log = []
+		for e in logs:
+			if logs[e]['user'] == target.id:
+				log = ACTION(logs[e]['lib'], 
+							 logs[e]['user'], 
+							 logs[e]['author'], 
+							 logs[e]['time'], 
+							 reason=logs[e]['reason'], 
+							 message=logs[e]['message'],
+							 log_id=logs[e]['log_id'],
+							 log_channel=logs[e]['log_channel'],
+							 log_content=logs[e]['log_content'],
+							 num=e)
+				list_log.append(log)
+		if not list_log:
+			return await ctx.channel.send(':x: Aucun log trouvé pour {}.'.format(target.name))
+
+		await ctx.channel.send(':pencil: {} logs trouvés pour {}.'.format(len(list_log), target.name))
+		
+		# Envoi des logs
+		if len(list_log) > 5:
+			for log in list_log:
+				EMB = log.embed(self.client, log.color)
+				await ctx.author.send(content=None, embed=EMB)
+			await ctx.channel.send(':mailbox: Logs expédiés.')
+		else:
+			for log in list_log:
+				EMB = log.embed(self.client, log.color)
+				await self.client.get_channel(self.id['TEXTCHANNEL']['LOG']).send(content=None, embed=EMB)
+				
+	@commands.command(name='clearlog', pass_context=True)
+	async def clear(self, ctx, numero_log: int):
+		""" Supprime le log
+		"""
+		# Clear log
+		logs = tool.get_data('src/config/moderation/data.json')
+		try:
+			logs.pop(str(numero_log))
+			await ctx.channel.send(':outbox_tray: Log n°{} supprimé avec succès'.format(numero_log))
+			tool.set_data(logs, 'src/config/moderation/data.json')
+		except:
+			await ctx.channel.send(':x: Log n°{} introuvable.'.format(numero_log))
+				
 	@commands.command(name='libre', pass_context=True)
 	async def libre(self, ctx):
 		await ctx.channel.send('```LIBRE```')
@@ -171,6 +225,15 @@ class Moderation:
 		""" Vérifie les logs des nouveaux arrivants
 		"""
 		await log('{} a rejoint le serveur'.format(member.name), MNT, time=True)
+		
+		logs = tool.get_data('src/config/moderation/data.json')
+		list_log = []
+		for e in logs:
+			if logs[e]['user'] == member.id:
+				list_log.append(e)
+		if not list_log:
+			return
+		await self.client.get_channel(self.id['TEXTCHANNEL']['MODER']).send(':star: {} a rejoint le serveur : {} logs.'.format(member.mention, len(list_log)))
 				
 	# ON MEMBER REMOVE	
 	async def on_member_remove(self, member):
@@ -199,13 +262,13 @@ class Moderation:
 		async for entry in guild.audit_logs(action=discord.AuditLogAction.kick, limit=20):
 			if datetime.utcnow() - entry.created_at > timedelta(0,10):
 				break
-			print(str(entry.target))
 			if entry.target == member:
 				await log("KICK manuel", MNT)
-				action = ACTION('Kick', entry.target, entry.user, entry.created_at, reason=entry.reason)
+				action = ACTION('Kick', entry.target.id, entry.user.id, entry.created_at, reason=entry.reason)
 				EMB = action.embed(self.client, 0x995500)
 				await self.client.get_channel(self.id['TEXTCHANNEL']['LOG']).send(content=None, embed=EMB)
 				await self.client.get_channel(self.id['TEXTCHANNEL']['MODER']).send(':hammer: {} a été kick du serveur.'.format(str(entry.target)))
+				action.save('src/config/moderation/data.json')
 				
 				# Check SDD
 				if check.is_role(member, self.sdd_role):
@@ -224,10 +287,11 @@ class Moderation:
 				break
 			if entry.target == member:
 				await log('BAN manuel', MNT)
-				action = ACTION('Ban', entry.target, entry.user, entry.created_at, reason=entry.reason)
+				action = ACTION('Ban', entry.target.id, entry.user.id, entry.created_at, reason=entry.reason)
 				EMB = action.embed(self.client, 0x550000)
 				await self.client.get_channel(self.id['TEXTCHANNEL']['LOG']).send(content=None, embed=EMB)
 				await self.client.get_channel(self.id['TEXTCHANNEL']['MODER']).send(':skull_crossbones: {} a été ban du serveur.'.format(str(entry.target)))
+				action.save('src/config/moderation/data.json')
 				
 				# Check SDD
 				if check.is_role(member, self.sdd_role):
@@ -262,7 +326,55 @@ class Moderation:
 		----
 		> Pour la SDD, on vérifie le nombre de personnes ayant déjà l'un des deux rôles. Si d'autres personnes ont déjà ce rôle, les nouveaux arrivants iront par défaut dans la seconde salle.
 		"""
-		return
+		guild = self.client.get_guild(GUILD_ID)
+		author = guild.get_member(payload.user_id)
+		channel = self.client.get_channel(payload.channel_id)
+		msg = await channel.get_message(payload.message_id)
+
+		# Checks
+		if not check.is_role(author, self.id['ROLE']['MODER']):
+			return
+		if check.is_role(msg.author, self.id['ROLE']['MODER']):
+			return
+		
+		# Emojis
+		emoji = payload.emoji
+		
+		# LOG :eyes:
+		if emoji.name == u"\U0001F440":
+			if not check.manage_messages_allowed(guild.me) or not check.manage_messages_allowed(author):
+				await log('Permission to remove emoji denied', MNT)
+				return
+			logs = tool.get_data('src/config/moderation/data.json')
+			for e in logs:
+				if logs[e]['user'] == msg.author.id and logs[e]['log_id'] == payload.message_id:
+					return
+			action = ACTION('LOG {}'.format(emoji), msg.author.id, author.id, msg.created_at, log_id=msg.id, log_channel=msg.channel.id, log_content=msg.content)
+			await msg.remove_reaction(emoji, author)
+			
+		
+		# DEL :x:
+		elif emoji.name == u"\U0000274C":
+			if not check.manage_messages_allowed(guild.me) or not check.manage_messages_allowed(author):
+				await log('Permission to remove emoji denied', MNT)
+				return
+			action = ACTION('LOG {}'.format(emoji), msg.author.id, author.id, msg.created_at, log_id=msg.id, log_channel=msg.channel.id, log_content=msg.content)
+			await msg.delete()
+		
+		# SDD :hole:
+		elif emoji.name == u"\U0001F573":
+			return
+		else:
+			return
+		
+		# Notification dans le salon de log
+		EMB = action.embed(self.client, action.color)
+		await self.client.get_channel(self.id['TEXTCHANNEL']['LOG']).send(content=None, embed=EMB)
+		
+		# Sauvegarde du log
+		action.save('src/config/moderation/data.json')
+				
+		
 	
 	# ON MEMBER BAN
 	async def on_member_ban(self, guild, user):
@@ -282,7 +394,6 @@ class Moderation:
 		elif check.is_role(before, self.sdd_role) and not check.is_role(after, self.sdd_role):
 			lost_role = [role for role in before.roles if role not in set(after.roles)]
 			await log('{} a perdu le rôle {}'.format(after.name, lost_role[0].name), MNT)
-			print(lost_role[0].id)
 			if lost_role[0].id in self.id['ROLE']['DIALO']:
 				channel = self.client.get_channel(self.id['TEXTCHANNEL']['DIALO'])
 			else:
