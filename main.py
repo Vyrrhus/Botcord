ce36f35e35df3a7cac7454346f7378c3f237da10
baptiste_desoubrie <baptiste.desoubrie@obspm.fr>
2023-07-31 16:34:13 +0200

    cogsManager, slash commands & move config

--- main.py
+++ main.py
@@ -1,143 +1,91 @@
 #!/opt/Botcord/.pyenv/bin/python
-# @author : Vyrrhus
 """
-BOT DE MODÉRATION DU DISCORD INSOUMIS
-- voir https://discordpy.readthedocs.io/en/stable/# -
-"""
+@author: Vyrrhus
 
+Documentation of discord.py is available at :
+> https://discordpy.readthedocs.io/en/stable/# -
+"""
 import discord
 from discord.ext import commands
 import asyncio
-import sys, traceback
-from utils import *
-from config.id import OWNER_ID
+from src import check
+from cogsManager import *
 
-##################
-# SETTINGS
-##################
+###############################################################################
+#   SETTINGS
+from config.config import TOKEN, PREFIX, ClassID
+from config.version import VERSION
 
-# TOKEN
-try:
-    TOKEN = read('config/token.config')
-except:
-    traceback.print_exc()
-    sys.exit()
+id = ClassID()
 
-# PREFIX
-try:
-    PREFIX = read('config/prefix.config')
-except:
-    traceback.print_exc()
-    PREFIX = '$'
+###############################################################################
+#   BOT
+BOTCORD = commands.Bot(
+    command_prefix=PREFIX,
+    owner_id=id.owner,
+    intents=discord.Intents.all()
+    )
 
-# VERSION
-from src.config.version import VERSION
-
-# BOT
-bot = commands.Bot(command_prefix=PREFIX, 
-                   intents=discord.Intents.all(),
-                   owner_id=OWNER_ID)
-
-##################
-# OWNER COMMANDS
-##################
-
-@bot.command(name='version', hidden=True)
+#------------------------------------------------------------------------------
+# SYNC COMMAND (to update slash commands to the command tree)
+@BOTCORD.command()
+@commands.guild_only()
 @commands.is_owner()
-async def _version(ctx):
-    await ctx.send(f"Version du bot : {VERSION}", reference=ctx.message)
+async def sync(ctx: commands.Context) -> None:
+    """ Sync slash commands to the guild"""
+    ctx.bot.tree.copy_global_to(guild=ctx.guild)
+    synced = await ctx.bot.tree.sync(guild=ctx.guild)
+    await ctx.send(
+        f"Synchronisation de {len(synced)} commandes sur ce serveur.",
+        ephemeral=True
+    )
 
-# LOGOUT
-@bot.command(name='logout', aliases=['close'], hidden=True)
-@commands.is_owner()
-async def _logout(ctx):
-    await Console(bot).print(f"Log out :zzz:")
-    await bot.close()
-
-# EXTENSIONS
-@bot.command(name='extension', hidden=True)
-@commands.is_owner()
-async def _extension(ctx):
-    extensions = read('config/extension.config', split=True)
-    if extensions:
-        await ctx.send(f":memo: {len(extensions)} extensions actives : {', '.join(extensions)}", reference=ctx.message)
-    else:
-        await ctx.send(f":x: Aucune extension active.", reference=ctx.message)
-
-@bot.command(name='enable', aliases=['activate', 'load'], hidden=True)
-async def _enable(ctx, extensions):
-    extensions = extensions.split(',')
-    
-    active = []
-    for ext in extensions:
-        try:
-            await bot.load_extension(f"cogs.{ext}")
-            active.append(ext)
-        except:
-            traceback.print_exc()
-    write_append('config/extension.config', active)
-    if len(active) > 0:
-        em = discord.Embed(color=ctx.author.color, description=f"Extension activée : **{', '.join(active)}**", timestamp=ctx.message.created_at)
-        await ctx.send(embed=em, reference=ctx.message)
-    else:
-        await ctx.send(f"{', '.join(extensions)} n'a pas pu être chargée", reference=ctx.message)
-
-@bot.command(name='disable', aliases=['desactivate', 'unload'], hidden=True)
-async def _disable(ctx, extensions):
-    if extensions == 'all' or extensions == '*':
-        extensions = read('config/extension.config', split=True)
-    else:
-        extensions = extensions.split(',')
-
-    inactive = []
-    for ext in extensions:
-        try:
-            await bot.unload_extension(f"cogs.{ext}")
-            inactive.append(ext)
-        except:
-            traceback.print_exc()
-        
-    write_remove('config/extension.config', inactive)
-    if len(inactive) > 0:
-        em = discord.Embed(color=ctx.author.color, description=f"Extension désactivée : **{', '.join(inactive)}**", timestamp=ctx.message.created_at)
-        await ctx.send(embed=em, reference=ctx.message)
-    else:
-        await ctx.send(f"{', '.join(extensions)} n'a pas pu être enlevée", reference=ctx.message)
-
-# COMMANDE
-@bot.command(name='command', hidden=True)
-@commands.is_owner()
-async def _command(ctx, *command):
-    print(f"Commande à exécuter : {' '.join(command)}")
-
-##################
-# MAIN EVENTS
-##################
-
-@bot.event
+#------------------------------------------------------------------------------
+# EVENT
+@BOTCORD.event
 async def on_ready():
-    await Console(bot).print(f"Bot ready : v{VERSION}")
-    print(f"Bot ready : v{VERSION}")
+    print(f"Logged in as {BOTCORD.user} (ID : {BOTCORD.user.id})")
+    print(f"------")
 
-##################
-# RUN
-##################
+#------------------------------------------------------------------------------
+# VERSION
+@BOTCORD.tree.command(name='version')
+@check.is_owner(id)
+async def _version(interaction: discord.Interaction):
+    """ Version du bot """
+    await interaction.response.send_message(
+        f"Version du bot : :robot: {VERSION}",
+        ephemeral=False
+        )
 
+#------------------------------------------------------------------------------
+# LOGOUT
+@BOTCORD.tree.command(name='logout')
+@check.is_owner(id)
+async def _logout(interaction: discord.Interaction):
+    """ Log out le bot """
+    await interaction.response.send_message(f"Log out :zzz:")
+    await BOTCORD.close()
+
+#------------------------------------------------------------------------------
+# COGS MANAGER
+@BOTCORD.tree.command(name='cogs')
+@check.is_owner(id)
+async def _cogs(interaction: discord.Interaction):
+    """ Cogs Manager """
+    manager = CogsManager(BOTCORD)
+    await CogsPaginator(interaction, manager).start()
+
+###############################################################################
+#   RUN
 async def start():
-    EXTENSIONS = read('config/extension.config', split=True)
-    for ext in EXTENSIONS:
-        try:
-            await bot.load_extension(f"cogs.{ext}")
-            print(f"Cog : [{ext}] actif")
-        except:
-            traceback.print_exc()
-            pass
+    """ Start BOTCORD """
+    manager = CogsManager(BOTCORD)
+    for cog in manager.cogs:
+        await manager.load(cog)
+        print(f"Cog [{cog}] activé au démarrage.")
     
-    try:
-        await bot.start(TOKEN)
-    except:
-        traceback.print_exc()
-        sys.exit()
+    await BOTCORD.start(TOKEN)
 
 if __name__ == '__main__':
     asyncio.run(start())
