8ea2823a53160ff17d0f398a12fd614ab375235f
baptiste_desoubrie <baptiste.desoubrie@obspm.fr>
2023-08-06 22:28:45 +0200

    fix docstring

--- cogs/setup.py
+++ cogs/setup.py
@@ -1,4 +1,5 @@
-""" Cette extension permet de modifier la configuration du bot :
+""" Cette extension permet de modifier la configuration du bot.
+    ▫️ **/setup** : configurer les channels & rôles
 """
 import discord
 from discord.ext import commands
